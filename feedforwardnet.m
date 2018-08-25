cbd7b94e71558d3736aae0b6964620629f4e9700
Djihad Boukara <djihad.boukara@gmail.com>
2018-08-25 20:32:59 +0200

    aggiunti gli script per la creazione di una rete feed forward multi-strato full connected e per eseguire la propagazione in avanti

---
+++ feedforwardnet.m
@@ -0,0 +1,30 @@
+function net = feedforwardnet(hiddenSizes, outFnc)
+% Crea una rete feed forward full connected
+%
+% hiddenSize: array contenente in numero di neuroni per ogni strato,
+%             il primo numero equivale a gli iput della rete
+%
+% outFnc: cell array contenente le funzioni di output di ogni strato
+%
+% output: struct che rappresenta la rete
+
+    % Salvo il numero di input e il numero di neuroni per ogni strato
+    net.hiddenSize = hiddenSizes;
+
+    % Salvo l'array contenente le funzioni dei vari strati
+    net.outFnc = outFnc;
+    
+    % len contiene il numero di strati della rete più 1 per l'input
+    len = length(hiddenSizes);
+    
+    for i = 2 : len
+        % per ciascuno strato creo una matrice di pesi tale per ogni 
+        % nodo dello strato (i-1)-esimo (colonna) ci siano una connesione
+        % con il nodo dello strato i-esimo (riga)
+        net.weights{i - 1} = rand(hiddenSizes(i),hiddenSizes(i-1));
+        
+        % Per ogni strato aggiungo un array di bias 
+        % uno per ogni neurone
+        net.biases{i-1} = rand(hiddenSizes(i), 1);
+    end
+end
---
+++ forwardpropagation.m
@@ -0,0 +1,59 @@
+function out = forwardpropagation(net, input)
+% Funzione che esegue la forward propagation e restituisce l'array
+% contenente l'otuput della rete
+%
+% net: rete neurale
+%
+% input: input della rete 
+    
+    % Numero di starti della rete + 1 per l'input
+    len = length(net.hiddenSize);
+    
+    % Array contenente l'output devari strati
+    z = cell(len);
+    
+    % La prima riga conterrà l'input dato alla rete
+    z{1} = input;
+    
+    % Per ogni strato della rete (1 è l'input)
+    for l = 2: len
+        
+        % Numero di nodi nello strato l
+        nodes = net.hiddenSize(l);
+        
+        % Valori di attivazione di ciascun nodo dello strato l
+        % al quale verrà applicata la funzione di output
+        % inizializzata con il valore di bias
+        in = net.biases{l - 1};
+        
+        % Array che conterrà gli output dei nodi dello strato l
+        z{l} = zeros(nodes);
+        
+        % Per ogni nodo dello strato l
+        for i = 1: nodes
+            
+            a = in(1);
+           
+            % Array contenente il peso delle connesioni entranti sul nodo i
+            w = net.weights{l-1}(i);
+            
+            % Per ogni connessione entrante nel nodo i dello strato l
+            for j = 1: length(w)
+                % Calcolo il contributo del nodo j dello strato l-1
+                a = a + w(j) * z{l-1}(j);
+            end
+            
+            % Estraggo la funzione dello strato l 
+            fnc = net.outFnc{l-1};
+            % Applico la funzione e salvo il riultato nell'array
+            z{l}(i) = fnc(a);
+           
+        end
+    end
+    
+    % Restituisco l'ultimo array di output
+    % equivalente all'output della rete
+    out = z{len};
+end
+
+
