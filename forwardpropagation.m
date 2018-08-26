ed4723764dc0befcaf597e69b6e7d093f4416547
djihad-boukara <djihad.boukara@outlook.com>
2018-08-26 18:14:35 +0200

    Update forwardpropagation.m
    Sostituiti i due cicli for interni con il prodotto matrice vettore.

--- forwardpropagation.m
+++ forwardpropagation.m
@@ -4,7 +4,7 @@ function out = forwardpropagation(net, input)
 %
 % net: rete neurale
 %
-% input: input della rete 
+% input: input della rete espersso come vettore colonna
     
     % Numero di starti della rete + 1 per l'input
     len = length(net.hiddenSize);
@@ -13,44 +13,31 @@ function out = forwardpropagation(net, input)
     z = cell(len);
     
     % La prima riga conterrà l'input dato alla rete
-    z{1} = input;
+    z{1} = [input; 1];
     
     % Per ogni strato della rete (1 è l'input)
     for l = 2: len
         
         % Numero di nodi nello strato l
-        nodes = net.hiddenSize(l);
+        nodes = net.hiddenSize(l) + 1;
         
         % Array che conterrà gli output dei nodi dello strato l
         z{l} = zeros(nodes);
         
-        % Per ogni nodo dello strato l
-        for i = 1: nodes
-            
-            % Valori di attivazione del nodo al quale verrà applicata 
-            % la funzione di output, inizializzata con il valore di bias
-            a = net.biases{l - 1}(i);
-           
-            % Array contenente il peso delle connesioni entranti sul nodo i
-            w = net.weights{l-1}(i);
-            
-            % Per ogni connessione entrante nel nodo i dello strato l
-            for j = 1: length(w)
-                % Calcolo il contributo del nodo j dello strato l-1
-                a = a + w(j) * z{l-1}(j);
-            end
-            
-            % Estraggo la funzione dello strato l 
-            fnc = net.outFnc{l-1};
-            % Applico la funzione e salvo il riultato nell'array
-            z{l}(i) = fnc(a);
-           
-        end
+        % Array contenente il peso delle connesioni entranti sul nodo i
+        w = net.weights{l-1};
+        % Calcolo il contributo del nodo j dello strato l-1
+        a =  w * z{l-1};
+        
+        % Estraggo la funzione dello strato l 
+        fnc = net.outFnc{l-1};
+        % Applico la funzione e salvo il riultato nell'array
+        z{l} = [fnc(a); 1];
     end
     
     % Restituisco l'ultimo array di output
-    % equivalente all'output della rete
+    % equivalente all'output della rete, rimuovendo l'ultimo valore
+    % usato come fattore moltiplicativo del bias
+    z{len}(nodes) = [];
     out = z{len};
 end
-
-
