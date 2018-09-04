1dd706e235593cd72b0b05d37a864163331dccea
Emanuele <ema.cio1793@gmail.com>
2018-09-04 13:46:44 +0200

    new backprop

--- backprop.m
+++ backprop.m
@@ -1,64 +1,59 @@
 function backprop(net, imagename, labelname)
+
+images = loadMNISTImages('train-images.idx3-ubyte');
+labels = loadMNISTLabels('train-labels.idx1-ubyte');
+
+% disp(images);
+% display_network(images(:,1:100)); % Show the first 100 images
+% disp(labels(1:10));
+
+N = size(images);
+N = N(2);
+
+% numero di strati della rete
+len = length(net.hiddenSize);
+
+targets = zeros(10, N);
+
+for i = 1: N
+    targets(labels(i, 1)+1, i) = 1;
+end
+
+
+for i = 1: 1
+    x = images(:, i);
+    delta = cell(len, 1);
+    y = forwardpropagation(net, x);
     
-    images = loadMNISTImages('train-images.idx3-ubyte');
-    labels = loadMNISTLabels('train-labels.idx1-ubyte');
-    
-    % disp(images);
-    % display_network(images(:,1:100)); % Show the first 100 images
-    % disp(labels(1:10));
-    
-    N = size(images);
-    N = N(2);
-    
-    % numero di strati della rete
-    len = length(net.hiddenSize);
-    
-    targets = zeros(10, N);
-  
-    for i = 1: N
-        targets(labels(i, 1)+1, i) = 1;
+    %Backprop
+    for k = 1 : net.hiddenSize(len)
+        delta{len}(k) = (y{len}(k) - targets(k));
     end
+    
+    for i = len - 1 : -1 : 2
         
-    
-    for i = 1: 1
-       x = images(:, i);       
-       delta = cell(len, 1);
-       y = forwardpropagation(net, x);
-       %TODO ADD BIAS
-       % disp(y{len});
-       % disp(targets(:,i))
-       deltaKout = y{len} - targets(:,i);        
-       % disp(deltaKout);
-       
-       %zHn = y(1:len-1);
-       %OneMinuszHn = 1-zHn;
-       %deltaKhidden = zHn .* OneMinuszHn;
-       
-       deltaKhidden = cell(len-2,1);
-       for j = 1:len-2
-           zHn=y{len-j};
-           OneMinuszHn = 1-zHn;
-           deltaKhidden{len-j-1}=zHn .* OneMinuszHn;
-       end
-       
-       last=deltaKout;
-       for j = 1:len-2
-           currLen = len-j;
-           wKh = net.weights{len-j};
-           temp=0;
-           for h = 1:net.hiddenSize(currLen)
-                %column=wKh(1:net.hiddenSize(currLen+1),h);
-                %disp(column);
-                %temp=temp+ sum((column.*last));
-                row=wKh(h,1:net.hiddenSize(currLen+1));
-                temp=sum((row * last));
-                disp(deltaKhidden{j}(h));
-                deltaKhidden{j}(h)=deltaKhidden{j}(h) * temp;
-           end
-           last=deltaKhidden;
-       end
-       
+        for j = 1 : net.hiddenSize(i)
+            
+            delta{i}(j) = y{i} * (1 - y{i});
+            
+            matrix = net.weights{i+1};
+            column = matrix(:, j);
+            
+            delta{i}(j) = sum(column .* delta{i+1}(j));
+        end
+        
     end
+    
+    for i = 1 : length(net.hiddenSize)
+        
+        for j = 1 : net.hiddenSize(i)
+            net.weights{i}(j) = net.weights{i}(j) - eta * delta{i}(j) * y{i}(j);
+        end
+        
+    end
+    
+    
+end
 end
 
 
