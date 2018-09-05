17295c8e17456935b14786616c06c844529e5a6a
djihad-boukara <djihad.boukara@outlook.com>
2018-09-05 16:40:58 +0200

    upadated forwardpropagation & add backprop2
    forwardpropagation  è stato modificaro ora restituisce solo gli output sela l'input.
    In backprop2 la prima riga dei delta non è più vuota

---
+++ backprop2.m
@@ -0,0 +1,81 @@
+function backprop(net)
+
+    images = loadMNISTImages('train-images.idx3-ubyte');
+    labels = loadMNISTLabels('train-labels.idx1-ubyte');
+
+    % disp(images);
+    % display_network(images(:,1:100)); % Show the first 100 images
+    % disp(labels(1:10));
+
+    eta = 0.5;
+
+    N = size(images);
+    N = N(2);
+
+    % numero di strati della rete
+    len = length(net.hiddenSize);
+
+    targets = zeros(10, N);
+
+    for i = 1: N
+        targets(labels(i, 1)+1, i) = 1;
+    end
+
+
+    for im = 1: 10
+
+        x = images(:, im);
+        delta = cell(len-1, 1);
+        outputLayers = forwardpropagation(net, x);
+
+        for i = 1: net.hiddenSize(end)
+            delta{end}(i) = (outputLayers{end}(i) - targets(i));
+        end
+    
+        % Per ogni layer calcolo il delta
+        for layer = len-1 : -1 : 2
+
+            for node = 1 : net.hiddenSize(layer)
+                % Calcolo delta
+                delta{layer-1}(node) = outputLayers{layer-1}(node) * (1 - outputLayers{layer-1}(node));
+                % Estraggo i pesi in uscita dal nodo
+                exitWeights = net.weights{layer}(:, node);
+                % Estraggo i delta del layer successivo
+                deltaNext =  delta{layer};
+                delta{layer-1}(node) = delta{layer-1}(node) * sum(exitWeights .* deltaNext');
+            end
+        end
+
+        % Aggiornamento pesi
+        for layer = 1 : length(net.hiddenSize) - 1
+
+            w = net.weights{layer};
+            col = size(w);
+            col = col(2);
+
+            for j = 1 : net.hiddenSize(layer+1)
+
+                for k = 1 : col
+                    x = net.weights{layer}(j,k);
+                    y = eta * delta{layer}(j) * outputLayers{layer}(j);
+                    z = x - y;
+                    
+                    disp(net.weights{layer}(j,k));
+                    disp(layer);
+                    disp(j);
+                    disp(k);
+                    disp(z);
+                    net.weights{layer}(j,k) = z;
+                end
+            end
+        end
+
+    end
+end
+
+
+
+
+
+
+
--- forwardpropagation.m
+++ forwardpropagation.m
@@ -14,27 +14,19 @@ function out = forwardpropagation(net, input)
 
     % La prima riga conterrà l'input dato alla rete
     z{1} = [input; 1];
-    
+
     % Per ogni strato della rete (1 è l'input)
     for l = 2: len
-        
-        % Numero di nodi nello strato l
-        nodes = net.hiddenSize(l) + 1;
-        
-        % Array contenente il peso delle connesioni entranti sul nodo i
-        w = net.weights{l-1};
-        % Calcolo il contributo del nodo j dello strato l-1
-        a =  w * z{l-1};
-        
-        % Estraggo la funzione dello strato l 
-        fnc = net.outFnc{l-1};
+        a =  net.weights{l-1} * z{l-1};
+        % Estraggo la funzione dello strato l
+        layerFunction = net.outFnc{l-1};
         % Applico la funzione e salvo il riultato nell'array
-        z{l} = [fnc(a); 1];
+        z{l} = [layerFunction(a); 1];
     end
     
     % Restituisco gli array di output
     % equivalente all'output della rete, rimuovendo l'ultimo valore
     % usato come fattore moltiplicativo del bias
-    z{len}(nodes) = [];
-    out = z;
+    z{len}(end) = [];
+    out = z(2:l);
 end
