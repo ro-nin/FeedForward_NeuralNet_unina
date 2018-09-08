38522dd3bb64ac8acbb5358e911315f879aafa7a
djihad-boukara <djihad.boukara@outlook.com>
2018-09-08 17:11:33 +0200

    test on test set

--- mnist_train.m
+++ mnist_train.m
@@ -1,34 +1,42 @@
 function out = mnist_train(net, epochs)
     
-images = loadMNISTImages('train-images.idx3-ubyte');
-labels = loadMNISTLabels('train-labels.idx1-ubyte');
-labels = labels';
+train_im = loadMNISTImages('train-images.idx3-ubyte');
+train_lb = loadMNISTLabels('train-labels.idx1-ubyte');
+train_lb = train_lb';
 
-labels(labels==0) = 10;                                
-labels = dummyvar(labels);
+train_lb(train_lb==0) = 10;                                
+train_lb = dummyvar(train_lb);
+
+test_im = loadMNISTImages('t10k-images.idx3-ubyte');
+test_lb = loadMNISTLabels('t10k-labels.idx1-ubyte');
+test_lb = test_lb';
+
+test_lb(test_lb==0) = 10;                                
+test_lb = dummyvar(test_lb);
 
 sizeoftrain = 100;
+sizeoftest = 100;
 
 for i = 1: epochs
     for im = 1: sizeoftrain
-        net = train(net, images(:, im)', labels(im, :)', 0.3);
+        net = train(net, train_im(:, im)', train_lb(im, :)', 0.3);
     end
 end
 
 guessed = 0;
 error = 0;
 
-for i = 1: sizeoftrain    
-    test = propagate(net, images(:, i)');    
+for i = 1: sizeoftest    
+    test = propagate(net, test_im(:, i)');    
     [val, idx] = max(test{end});
     %fprintf("val: %.2f - idx: %d\n", val, idx);    
-    if(idx == find(labels(i, :)))
+    if(idx == find(test_lb(i, :)))
         guessed = guessed + 1;
     end   
-    error = error + 0.5 * sum((test{end} - labels(i,:)').^2);    
+    error = error + 0.5 * sum((test{end} - test_lb(i,:)').^2);    
 end
 
-fprintf("guessesed: %d/%d rate: %.2f%%\n", guessed, sizeoftrain,(guessed/sizeoftrain) * 100);
+fprintf("guessesed: %d/%d rate: %.2f%%\n", guessed, sizeoftest,(guessed/sizeoftest) * 100);
 fprintf("total error: %.2f\n", error);
 
 %display_network(images(:,1:100));
