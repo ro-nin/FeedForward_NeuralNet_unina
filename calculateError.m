df1e617541f19137f42ffa4e5281b774503e3508
Emanuele <ema.cio1793@gmail.com>
2019-01-22 12:42:15 +0100

    docu, bozza finale, da correggere

--- calculateError.m
+++ calculateError.m
@@ -1,17 +1,10 @@
 function output = calculateError(out, lb, errorFnc) 
-%Evaluate mean error, variance and then standard deviation ????
-%out: contains all the outputs layers coming from the test made
+
+%output: contains all the outputs layers coming from the test made
 %lb: contains the label matrix
 %errorFnc: the specified error formula relative to the error function used
 %   on the output layer
-N = size(out, 1);
 errors = errorFnc(out, lb);
-
-%????
-%error on multiple tests
-mu = sum(errors) / N;
-variance = sum((errors - mu) .^ 2) / N;
-deviation = sqrt(variance);
 
 output=sum(errors);
 end
